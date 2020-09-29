5f4da5bd2e7eb9b51e7c2dd83f9efb958e5c48b3
omarlopez2001 <omarlopez9445@gmail.com>
2020-09-29 11:54:44 -0400

    displayed cart items on console

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -32,12 +32,41 @@ for (let i=0; i<data.length; ++i) {
 const cart = []
 
 function addItem(name, price) {
-    const item = {name: name, price: price, qty: 1}
+    for (let i = 0; i < cart.length; i +=1) {
+        if (cart[i].name === name) {
+            cart[i].qty += 1
+            return
+        }
+    }
+    const item = {name, price, qty: 1}
     cart.push(item)
 }
 
 function showItems() {
-    console.log(`You have ${cart.length} items in your cart`)
+    const qty = getQty()
+    console.log(`You have ${getQty()} items in your cart`)
+
+    for (let i = 0; i < cart.length; i += 1) {
+        console.log(`- ${cart[i].name} $${cart[i].price} x ${cart[i].qty}`)
+    }
+
+    console.log(`Total in cart: $${getTotal()}`)
+}
+
+function getQty() {
+    let qty = 0 
+    for (let i = 0; i <cart.length; i +=1) {
+        qty += cart[i].qty
+    }
+    return qty
+}
+
+function getTotal() {
+    let total = 0
+    for (let i = 0; i < cart.length; i +=1) {
+        total += cart[i].price * cart[i].qty
+    }
+    return total.toFixed(2)
 }
 
 addItem('Apple', 0.99)
