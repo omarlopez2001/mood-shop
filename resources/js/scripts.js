a5f2ab23d7ea8237a33d7c29aa38e46bf63343e8
omarlopez2001 <omarlopez9445@gmail.com>
2020-09-29 23:53:44 -0400

    add button connected to cart

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -15,13 +15,13 @@ for (let i=0; i<data.length; ++i) {
     let desc = document.createElement('P')
     desc.innerText =data[i].desc
     newDiv.appendChild(desc)
+
     let price = document.createElement('P')
     price.innerText = data[i].price
     newDiv.appendChild(price)
 
     let button = document.createElement('button')
     button.id = data[i].name
-
     button.dataset.price = data[i].price
     button.innerHTML = "Add to Cart"
     newDiv.appendChild(button)
@@ -35,12 +35,33 @@ const cartTotal = document.getElementById('cart-total')
 itemList.innerHTML = '<li> Hello World</li>'
 console.log(itemList)
 
+itemList.click = function(e){
+    if(e.target & e.target.classList.contains('remove')){
+        const name = e.target.dataset.name
+        removeItem(name)
+    } else if (e.target & e.target.classList.contains('add-one')){
+        const name = e.target.dataset.name
+        addItem(name)
+    } else if (e.target & e.target.classList.contains('remove-one')){
+        const name = e.target.dataset.name
+        removeItem(name, 1)
+    }
+}
+
+const all_items_button = Array.from(document.querySelectorAll("button"))
+
+all_items_button.forEach(elt => elt.addEventListener('click', () => {
+    addItem(elt.getAttribute('id'), elt.getAttribute('data-price'))
+    showItems()
+  }))
+
 const cart = []
 
 function addItem(name, price) {
     for (let i = 0; i < cart.length; i +=1) {
         if (cart[i].name === name) {
             cart[i].qty += 1
+            showItems()
             return
         }
     }
@@ -64,7 +85,6 @@ function showItems() {
         itemStr += `<li> ${name} $${price} x ${qty} = ${qty * price}</li>`
     }
     itemList.innerHTML = itemStr
-
     // console.log(`Total in cart: $${getTotal()}`)
     cartTotal.innerHTML = `Total in cart: $${getTotal()}`
 }
@@ -94,6 +114,7 @@ function removeItem(name, qty = 0) {
             if (cart[i].qty < 1 || qty === 0) {
                 cart.splice(i, 1)
             }
+            showItems()
             return
         }
     }
